b9211a9480f8a9eb17f8359e503f90e9fdb6c1ae
Eduardo Tejeda <eduardotejedadelgado@gmail.com>
2023-05-07 10:16:10 -0400

    Add style and logic to dropdown

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,35 +1,45 @@
 import { useState } from 'react';
+import { GoChevronDown } from 'react-icons/go';
+import Panel from './Panel';
 
-function Dropdown({options, selection, onSelect}) {
-    const [isOpen, setIsOpen] = useState(false);
+function Dropdown({ options, value, onChange }) {
+  const [isOpen, setIsOpen] = useState(false);
 
-    const handleClick = () => {
-        setIsOpen(!isOpen);
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
 
-    };
+  const handleOptionClick = (option) => {
+    // CLOSE DROPDOWN
+    setIsOpen(false);
+    // WHAT OPTION DID THE USER CLICK ON???
+    onChange(option);
+  };
 
-    const handleOptionClick = (selecterOption) => {
-        setIsOpen(false);
+  const renderedOptions = options.map((option) => {
+    return (
+      <div
+        className="hover:bg-sky-100 rounded cursor-pointer p-1"
+        onClick={() => handleOptionClick(option)}
+        key={option.value}
+      >
+        {option.label}
+      </div>
+    );
+  });
 
-        onSelect(options);
-    };
-
-    const renderedOptions = options.map((option) => {
-        return <div onClick={() => handleOptionClick(option)} key={option.value}>
-            {option.label}
-        </div>
-    })
-
-    let content = 'Select...';
-    if (selection) {
-        content = selection.label;
-    }
-
-
-    return <div>
-    <div onClick={handleClick}>{content}</div>
-    {isOpen && <div>{renderedOptions}</div>}
-    </div>;
+  return (
+    <div className="w-48 relative">
+      <Panel
+        className="flex justify-between items-center cursor-pointer"
+        onClick={handleClick}
+      >
+        {value?.label || 'Select...'}
+        <GoChevronDown className="text-lg" />
+      </Panel>
+      {isOpen && <Panel className="absolute top-full">{renderedOptions}</Panel>}
+    </div>
+  );
 }
 
 export default Dropdown;
