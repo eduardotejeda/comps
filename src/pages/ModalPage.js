a63c9971930c8e1c4cf61f6cb4774e630838906b
Eduardo Tejeda <eduardotejedadelgado@gmail.com>
2023-05-11 05:16:45 -0400

    Finish modal example

--- src/pages/ModalPage.js
+++ src/pages/ModalPage.js
@@ -6,16 +6,30 @@ function ModalPage() {
 
     const [showModal, setShowModal] = useState(false);
 
-    const handleClick = () => {
+        const handleClick = () => {
         setShowModal(true);
     };
 
+    const handleClose = () => {
+        setShowModal(false);
 
+    };
+
+    
+    const actionBar = <div><Button onClick={handleClose} primary>I Accept</Button> </div>
+    const modal = <Modal onClose={handleClose} actionBar={actionBar}> 
+    <p>
+        Here is an important agrrement for you to accept.
+    </p>
+    </Modal>
 
     return  (
         <div>
-            <Button onClick={handleClick} primary>Open Modal</Button>
-            {showModal && <Modal/>}
+            <Button onClick={handleClick} primary>
+                Open Modal
+            </Button>
+
+            {showModal && modal}
         </div>
     )
 
