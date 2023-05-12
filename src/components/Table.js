ddf3a1ba55dfde684925760face0a444f1b1f681
Eduardo Tejeda <eduardotejedadelgado@gmail.com>
2023-05-11 20:58:07 -0400

    Add table logic

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,10 +1,18 @@
-function Table({data}) { 
-    const renderedRows = data.map((fruit) => {
+function Table({data, config, keyFn}) { 
+    const renderedHeaders = config.map((column)=>{
+        return <th key={column.label}>{column.label}</th>
+    })
+
+
+    const renderedRows = data.map((rowData) => {
+        const renderedCells = config.map((column) => {
+            return <td className="p-2" key={column.label}>{column.render(rowData)}</td>
+
+        });
+
         return (
-            <tr className="border-b" key={fruit.name}>
-                <td className="p-3">{fruit.name}</td>
-                <td className="p-3"><div className={`p-3 m-2 ${fruit.color}`}></div></td>
-                <td className="p-3">{fruit.score}</td>
+            <tr className="border-b" key={keyFn(rowData)}>
+                {renderedCells}
             </tr>
         )
 
@@ -15,9 +23,7 @@ function Table({data}) {
     <table className="table-auto border-spacing-2">  
         <thead>
             <tr className="border-b-2">
-                <th>Fruit</th>
-                <th>Color</th>
-                <th>Score</th>
+                {renderedHeaders}
             </tr>
         </thead>
         <tbody>
